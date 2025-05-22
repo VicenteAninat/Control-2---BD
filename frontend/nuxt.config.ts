126879982de66f5f4084f8d4a1afad55cbcfefa0
Duvan Vergara <duvan.vergara@usach.cl>
2025-05-21 22:31:10 -0400

    Actualizaciones Frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  pages: true,
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      backendServer: process.env.BACKEND_SERVER,
+      backendPort: process.env.BACKEND_PORT
+    }
+  }
 })
---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container" style="text-align:center; margin-top: 80px;">
+    <h1 class="title">Bienvenido al Home</h1>
+    <nuxt-link to="/login" class="btn btn-primary" style="margin-top: 30px;">Iniciar Sesión</nuxt-link>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  middleware: 'auth'
+})
+</script>
