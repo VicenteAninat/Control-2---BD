59294c8fbbd67f6b9a8dee3184e9491085a92e76
Duvan Vergara <duvan.vergara@usach.cl>
2025-05-21 22:21:33 -0400

    Actualizaciones Frontend

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <header>
+      <h1>Mi Aplicación</h1>
+      <nav>
+        <nuxt-link to="/" class="btn btn-link">Inicio</nuxt-link>
+        <nuxt-link to="/login" class="btn btn-link">Iniciar Sesión</nuxt-link>
+      </nav>
+    </header>
+    <main>
+      <nuxt-page />
+    </main>
+    <footer>
+      <p>© 2025 Mi Aplicación</p>
+    </footer>
   </div>
 </template>
+
+
+<script setup>
+import { handleLogout } from './src/services/authService'
+import { useNuxtApp } from '#app'
+import { useRouter } from 'vue-router'
+
+const { $apiClient } = useNuxtApp()
+const router = useRouter()
+
+const logoutUser = async () => {
+  await handleLogout($apiClient, router)
+}
+
+</script>
